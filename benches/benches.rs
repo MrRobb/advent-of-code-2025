6bccee55e7b9f6784e934d6b36ba25b5fe6e6454
Mr.Robb <extremisinfinitum@gmail.com>
2025-12-01 08:17:01 +0000

    Add Day 1

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,11 +1,11 @@
 use std::fs::read_to_string;
 
 #[allow(clippy::wildcard_imports)]
-use advent_of_code_2022::*;
+use advent_of_code_2025::*;
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn bench1(c: &mut Criterion) {
-    let input01 = read_to_string("input/day01.txt").expect("Input file not found");
+    let input01 = day01::parse(&read_to_string("input/day01.txt").expect("Input file not found"));
     c.bench_function("Day 1 | Part 1", |b| b.iter(|| day01::zero_ticks_end(&input01)));
     c.bench_function("Day 1 | Part 2", |b| b.iter(|| day01::zero_ticks_any(&input01)));
 }
--- src/day01.rs
+++ src/day01.rs
@@ -1,21 +1,21 @@
 
-enum Direction {
-  L(u64),
-  R(u64)
+pub enum Direction {
+  L(i64),
+  R(i64)
 }
 
-fn parse(input: &str) -> Vec<Direction> {
+pub fn parse(input: &str) -> Vec<Direction> {
   input.lines().map(|line| {
     match line.chars().next().unwrap() {
-      'L' => Direction::L(line[1..].parse::<u64>().unwrap()),
-      'R' => Direction::R(line[1..].parse::<u64>().unwrap()),
+      'L' => Direction::L(line[1..].parse::<i64>().unwrap()),
+      'R' => Direction::R(line[1..].parse::<i64>().unwrap()),
       _ => panic!("Invalid direction"),
     }
   }).collect()
 }
 
-fn zero_ticks_end(input: Vec<Direction>) -> u64 {
-    let mut location = 50;
+pub fn zero_ticks_end(input: &Vec<Direction>) -> i64 {
+    let mut location: i64 = 50;
     let mut zero_ticks = 0;
     for direction in input {
         location = match direction {
@@ -29,13 +29,13 @@ fn zero_ticks_end(input: Vec<Direction>) -> u64 {
     zero_ticks
 }
 
-fn zero_ticks_any(input: Vec<Direction>) -> u64 {
-    let mut location = 50;
+pub fn zero_ticks_any(input: &Vec<Direction>) -> i64 {
+    let mut location: i32 = 50;
     let mut zero_ticks = 0;
     for direction in input {
       match direction {
         Direction::L(amount) => {
-          for _ in 0..amount {
+          for _ in 0..*amount {
             location = (location - 1).rem_euclid(100);
             if location == 0 {
                 zero_ticks += 1;
@@ -43,7 +43,7 @@ fn zero_ticks_any(input: Vec<Direction>) -> u64 {
           }
         },
         Direction::R(amount) => {
-          for _ in 0..amount {
+          for _ in 0..*amount {
             location = (location + 1).rem_euclid(100);
             if location == 0 {
                 zero_ticks += 1;
@@ -58,7 +58,7 @@ fn zero_ticks_any(input: Vec<Direction>) -> u64 {
 pub fn main() {
     let input = std::fs::read_to_string("input/day01.txt").expect("Input file not found");
     let now = std::time::Instant::now();
-    println!("PART 1 = {}", zero_ticks_end(&input));
-    println!("PART 2 = {}", zero_ticks_any(&input));
+    println!("PART 1 = {}", zero_ticks_end(&parse(&input)));
+    println!("PART 2 = {}", zero_ticks_any(&parse(&input)));
     println!("Execution time: {:?}", now.elapsed());
 }
