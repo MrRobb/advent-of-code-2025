577f46ea326c483292430482ad31d613627213db
Mr.Robb <extremisinfinitum@gmail.com>
2025-12-03 05:57:52 +0000

    Format day 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,17 +1,17 @@
-
 pub enum Direction {
-  L(i64),
-  R(i64)
+    L(i64),
+    R(i64),
 }
 
 pub fn parse(input: &str) -> Vec<Direction> {
-  input.lines().map(|line| {
-    match line.chars().next().unwrap() {
-      'L' => Direction::L(line[1..].parse::<i64>().unwrap()),
-      'R' => Direction::R(line[1..].parse::<i64>().unwrap()),
-      _ => panic!("Invalid direction"),
-    }
-  }).collect()
+    input
+        .lines()
+        .map(|line| match line.chars().next().unwrap() {
+            'L' => Direction::L(line[1..].parse::<i64>().unwrap()),
+            'R' => Direction::R(line[1..].parse::<i64>().unwrap()),
+            _ => panic!("Invalid direction"),
+        })
+        .collect()
 }
 
 pub fn zero_ticks_end(input: &Vec<Direction>) -> i64 {
@@ -19,8 +19,8 @@ pub fn zero_ticks_end(input: &Vec<Direction>) -> i64 {
     let mut zero_ticks = 0;
     for direction in input {
         location = match direction {
-          Direction::L(amount) => (location - amount).rem_euclid(100),
-          Direction::R(amount) => (location + amount).rem_euclid(100),
+            Direction::L(amount) => (location - amount).rem_euclid(100),
+            Direction::R(amount) => (location + amount).rem_euclid(100),
         };
         if location == 0 {
             zero_ticks += 1;
@@ -33,24 +33,24 @@ pub fn zero_ticks_any(input: &Vec<Direction>) -> i64 {
     let mut location: i32 = 50;
     let mut zero_ticks = 0;
     for direction in input {
-      match direction {
-        Direction::L(amount) => {
-          for _ in 0..*amount {
-            location = (location - 1).rem_euclid(100);
-            if location == 0 {
-                zero_ticks += 1;
-            }
-          }
-        },
-        Direction::R(amount) => {
-          for _ in 0..*amount {
-            location = (location + 1).rem_euclid(100);
-            if location == 0 {
-                zero_ticks += 1;
-            }
-          }
-        },
-      }
+        match direction {
+            Direction::L(amount) => {
+                for _ in 0..*amount {
+                    location = (location - 1).rem_euclid(100);
+                    if location == 0 {
+                        zero_ticks += 1;
+                    }
+                }
+            },
+            Direction::R(amount) => {
+                for _ in 0..*amount {
+                    location = (location + 1).rem_euclid(100);
+                    if location == 0 {
+                        zero_ticks += 1;
+                    }
+                }
+            },
+        }
     }
     zero_ticks
 }
